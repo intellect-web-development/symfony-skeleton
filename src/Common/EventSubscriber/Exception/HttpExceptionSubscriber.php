24275fc8de8144c319ab350e8c1310ea065e3326
alexander-erin <a.erin@fkcore.com>
2023-01-13 12:25:27 +0300

    add logger to http

--- src/Common/EventSubscriber/Exception/HttpExceptionSubscriber.php
+++ src/Common/EventSubscriber/Exception/HttpExceptionSubscriber.php
@@ -8,6 +8,7 @@ use App\Common\Exception\Domain\DomainException;
 use Exception;
 use IWD\Symfony\PresentationBundle\Dto\Output\ApiFormatter;
 use IWD\Symfony\PresentationBundle\Exception\PresentationBundleException;
+use Psr\Log\LoggerInterface;
 use Symfony\Component\EventDispatcher\Attribute\AsEventListener;
 use Symfony\Component\HttpFoundation\File\Exception\AccessDeniedException;
 use Symfony\Component\HttpFoundation\Response;
@@ -16,24 +17,40 @@ use Symfony\Component\HttpKernel\KernelEvents;
 use Symfony\Component\Serializer\SerializerInterface;
 use Throwable;
 
+#[AsEventListener(event: KernelEvents::EXCEPTION, method: 'logException', priority: 2)]
 #[AsEventListener(event: KernelEvents::EXCEPTION, method: 'onFormatterException', priority: 1)]
 class HttpExceptionSubscriber
 {
     public function __construct(
         private readonly SerializerInterface $serializer,
+        private readonly LoggerInterface $logger,
         private readonly string $env,
         private readonly bool $debug,
     ) {
     }
 
+    public function logException(ExceptionEvent $event): void
+    {
+        $exception = $event->getThrowable();
+        try {
+            throw $exception;
+        } catch (DomainException $exception) {
+            $this->logger->warning($exception->getMessage());
+        } catch (Throwable $exception) {
+            $this->logger->error($exception->getMessage());
+        }
+    }
+
     public function onFormatterException(ExceptionEvent $event): void
     {
         $format = (string) $event->getRequest()->attributes->get('_format', 'json');
+        $event->allowCustomResponseCode();
 
         $exception = $event->getThrowable();
         if ($exception instanceof AccessDeniedException) {
             return;
         }
+
         try {
             $previous = $exception->getPrevious();
             if (!empty($previous) && is_subclass_of($previous, DomainException::class)) {
@@ -49,6 +66,7 @@ class HttpExceptionSubscriber
                     $format
                 )
             );
+            $response->setStatusCode(Response::HTTP_UNPROCESSABLE_ENTITY);
             $response->headers->add(['Content-Type' => 'application/' . $format]);
             $event->setResponse($response);
         } catch (\Doctrine\DBAL\Exception $exception) {
@@ -68,6 +86,7 @@ class HttpExceptionSubscriber
                     )
                 );
                 $response->headers->add(['Content-Type' => 'application/' . $format]);
+                $response->setStatusCode(Response::HTTP_BAD_REQUEST);
                 $event->setResponse($response);
             }
         } catch (Throwable $exception) {
@@ -86,6 +105,7 @@ class HttpExceptionSubscriber
                         $format
                     )
                 );
+                $response->setStatusCode(Response::HTTP_INTERNAL_SERVER_ERROR);
                 $response->headers->add(['Content-Type' => 'application/' . $format]);
                 $event->setResponse($response);
             }
