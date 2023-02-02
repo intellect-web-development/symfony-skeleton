1810ab1b38d32f27640ccc91b75c26ca8cc5c32a
alexander <a.erin@fkcore.com>
2023-02-02 12:08:44 +0300

    chore

--- src/Auth/Entry/Console/CreateAdminCommand.php
+++ src/Auth/Entry/Console/CreateAdminCommand.php
@@ -4,35 +4,22 @@ declare(strict_types=1);
 
 namespace App\Auth\Entry\Console;
 
-use App\Auth\Core\User\Domain\User;
-use App\Auth\Core\User\Domain\UserRepository;
-use DateTimeImmutable;
-use Doctrine\ORM\EntityManagerInterface;
+use App\Auth\Core\User\Application\UseCase\Create\Command;
+use App\Auth\Core\User\Application\UseCase\Create\Handler;
 use Symfony\Component\Console\Attribute\AsCommand;
-use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Command\Command as CliCommand;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
-use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 #[AsCommand(name: 'app:auth:user:create-admin')]
-class CreateAdminCommand extends Command
+class CreateAdminCommand extends CliCommand
 {
-    private EntityManagerInterface $em;
-    private UserPasswordHasherInterface $passwordEncoder;
-    private UserRepository $userRepository;
-
     public function __construct(
-        EntityManagerInterface $em,
-        UserPasswordHasherInterface $passwordEncoder,
-        UserRepository $userRepository,
+        private readonly Handler $handler,
     ) {
         parent::__construct();
-
-        $this->em = $em;
-        $this->passwordEncoder = $passwordEncoder;
-        $this->userRepository = $userRepository;
     }
 
     protected function configure(): void
@@ -51,35 +38,16 @@ class CreateAdminCommand extends Command
         $password = $input->getOption('password');
         $name = $input->getOption('name');
 
-        if (!$password) {
-            $password = bin2hex(random_bytes(8));
-            $io->note(sprintf('Using generated password: %s', $password));
-        }
-
-        if ($this->userRepository->findOneBy(['email' => $email])) {
-            $io->error('Root user is already exists');
-
-            return Command::FAILURE;
-        }
-
-        $now = new DateTimeImmutable();
-        $user = User::create(
-            id: $this->userRepository->nextId(),
-            createdAt: $now,
-            updatedAt: $now,
-            email: $email,
-            roles: [User::ROLE_ADMIN],
-            name: $name,
+        $this->handler->handle(
+            new Command(
+                plainPassword: $password,
+                name: $name,
+                email: $email,
+            )
         );
-        $user->changePassword(
-            $this->passwordEncoder->hashPassword($user, $password)
-        );
-
-        $this->em->persist($user);
-        $this->em->flush();
 
         $io->success('Administration user was created!');
 
-        return Command::SUCCESS;
+        return CliCommand::SUCCESS;
     }
 }
