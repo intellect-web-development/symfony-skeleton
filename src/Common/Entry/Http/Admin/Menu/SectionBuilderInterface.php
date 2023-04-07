fa743a7d254f58b63f5019fcee018af51bbe5c90
alexander <sanyaerin@mail.ru>
2023-04-07 20:20:08 +0300

    chore

--- src/Common/Entry/Http/Admin/Menu/SectionBuilderInterface.php
+++ src/Common/Entry/Http/Admin/Menu/SectionBuilderInterface.php
@@ -5,7 +5,9 @@ declare(strict_types=1);
 namespace App\Common\Entry\Http\Admin\Menu;
 
 use Knp\Menu\ItemInterface;
+use Symfony\Component\DependencyInjection\Attribute\AutoconfigureTag;
 
+#[AutoconfigureTag('app.section_builder')]
 interface SectionBuilderInterface
 {
     public function build(ItemInterface $menu): void;
