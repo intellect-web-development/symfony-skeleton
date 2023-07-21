a9c1456f7666fb2baca1b4c2030ba87b50a6ca10
alexander <sanyaerin@mail.ru>
2023-07-21 11:03:47 +0300

    chore

--- src/Common/Service/FormTransformer/JsonTransformer.php
+++ src/Common/Service/FormTransformer/JsonTransformer.php
@@ -5,12 +5,14 @@ declare(strict_types=1);
 namespace App\Common\Service\FormTransformer;
 
 use JMS\Serializer\SerializerInterface;
+use JsonException;
 use Symfony\Component\Form\DataTransformerInterface;
+use Symfony\Component\Form\Exception\TransformationFailedException;
 
-class JsonTransformer implements DataTransformerInterface
+readonly class JsonTransformer implements DataTransformerInterface
 {
     public function __construct(
-        private readonly SerializerInterface $serializer,
+        private SerializerInterface $serializer,
     ) {
     }
 
@@ -27,8 +29,7 @@ class JsonTransformer implements DataTransformerInterface
     }
 
     /**
-     * @param  string|null    $value
-     * @throws \JsonException
+     * @param string|null $value
      */
     public function reverseTransform($value): ?array
     {
@@ -37,6 +38,14 @@ class JsonTransformer implements DataTransformerInterface
             return null;
         }
 
-        return json_decode($value, true, 512, JSON_THROW_ON_ERROR);
+        try {
+            return json_decode($value, true, 512, JSON_THROW_ON_ERROR);
+        } catch (JsonException $jsonException) {
+            $failure = new TransformationFailedException(message: $jsonException->getMessage(), previous: $jsonException);
+            $failure->setInvalidMessage('JSON parse Error', [
+                '{{ value }}' => $value,
+            ]);
+            throw $failure;
+        }
     }
 }
