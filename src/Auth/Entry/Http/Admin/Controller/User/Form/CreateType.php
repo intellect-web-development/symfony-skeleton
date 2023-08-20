20d500c96ef0c2c08d41d574be5907b1bcd7a11c
alexander <sanyaerin@mail.ru>
2023-08-20 12:23:19 +0300

    chore

--- src/Auth/Entry/Http/Admin/Controller/User/Form/CreateType.php
+++ src/Auth/Entry/Http/Admin/Controller/User/Form/CreateType.php
@@ -15,9 +15,15 @@ use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Validator\Constraints\Email;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Contracts\Translation\TranslatorInterface;
 
 class CreateType extends AbstractType
 {
+    public function __construct(
+        private readonly TranslatorInterface $translator
+    ) {
+    }
+
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
@@ -41,7 +47,7 @@ class CreateType extends AbstractType
             ->add('plainPassword', RepeatedType::class, [
                 'type' => PasswordType::class,
                 'label' => 'app.admin.ui.modules.auth.user.properties.password',
-                'invalid_message' => 'password_is_not_equals',
+                'invalid_message' => $this->translator->trans('app.admin.ui.modules.auth.user.flash.password_is_not_equals'),
                 'required' => true,
                 'first_options' => [
                     'label' => 'app.admin.ui.modules.auth.user.properties.password',
@@ -49,7 +55,7 @@ class CreateType extends AbstractType
                         new NotBlank(allowNull: false),
                         new Length([
                             'min' => 6,
-                            'minMessage' => 'password_is_short',
+                            'minMessage' => $this->translator->trans('app.admin.ui.modules.auth.user.flash.password_is_short'),
                             'max' => 4096,
                         ]),
                     ],
@@ -60,7 +66,7 @@ class CreateType extends AbstractType
                         new NotBlank(allowNull: false),
                         new Length([
                             'min' => 6,
-                            'minMessage' => 'password_is_short',
+                            'minMessage' => $this->translator->trans('app.admin.ui.modules.auth.user.flash.password_is_short'),
                             'max' => 4096,
                         ]),
                     ],
