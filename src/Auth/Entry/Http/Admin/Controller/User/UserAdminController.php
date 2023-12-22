58a0e162fcd91a69958d3bed1d7e23b4c36a94ab
alexander <sanyaerin@mail.ru>
2023-12-22 15:49:57 +0300

    wip

---
+++ src/Auth/Entry/Http/Admin/Controller/User/UserAdminController.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Auth\Entry\Http\Admin\Controller\User;
+
+use Sonata\AdminBundle\Controller\CRUDController;
+
+final class UserAdminController extends CRUDController
+{
+}
