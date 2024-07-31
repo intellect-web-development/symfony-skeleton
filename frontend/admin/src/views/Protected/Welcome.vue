373b2eafb4389e70eb4cd114519b6fad4e7567b0
alexander <sanyaerin@mail.ru>
2024-07-31 18:02:55 +0300

    wip

--- frontend/admin/src/views/Protected/Welcome.vue
+++ frontend/admin/src/views/Protected/Welcome.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="container justify-content-center">
+  <div class="container container-center">
     <div class="info">
       <Card class="card">
         <template #title>
