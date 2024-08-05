14af8a13c25da995d67cfde76a7d0d3924a8421b
alexander <sanyaerin@mail.ru>
2024-08-05 13:14:54 +0300

    wip

--- frontend/admin/src/views/Protected/Welcome/Welcome.vue
+++ frontend/admin/src/views/Protected/Welcome/Welcome.vue
@@ -6,7 +6,19 @@
           <p class="card__title">{{ $t(tPath + 'title') }}</p>
         </template>
         <template #content>
-          <span class="card__content">{{ $t(tPath + 'content') }}</span>
+          <p class="card__example">
+            <span class="card__example-demo0">*</span>
+            <span class="card__example-demo1">*</span>
+            <span class="card__example-demo2">*</span>
+          </p>
+          <p class="card__content">
+            {{ $t(tPath + 'content') }}
+          </p>
+          <p class="card__example">
+            <span class="card__example-demo0">*</span>
+            <span class="card__example-demo1">*</span>
+            <span class="card__example-demo2">*</span>
+          </p>
         </template>
       </Card>
     </div>
@@ -35,16 +47,29 @@ export default {
     @include size(12);
 
     background: var(--app-black-25);
-    p {
-      text-align: center;
-    }
 
     &__title {
       color: var(--green-700);
+      text-align: center;
     }
 
     &__content {
+      color: var(--blue-600);
+      text-align: center;
+    }
+
+    &__example {
+      display: flex;
+      flex-direction: row;
+      justify-content: space-between;
       color: var(--green-700);
+
+      &-demo1 {
+        color: var(--red-700);
+      }
+      &-demo2 {
+        color: var(--blue-700);
+      }
     }
   }
 }
