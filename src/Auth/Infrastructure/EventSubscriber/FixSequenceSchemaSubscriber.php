255653ac3612ad8a5009f96746b2756be82d388c
alexander <sanyaerin@mail.ru>
2024-10-21 13:15:25 +0300

    wip

--- src/Auth/Infrastructure/EventSubscriber/FixSequenceSchemaSubscriber.php
+++ src/Auth/Infrastructure/EventSubscriber/FixSequenceSchemaSubscriber.php
@@ -6,12 +6,13 @@ namespace App\Auth\Infrastructure\EventSubscriber;
 
 use Doctrine\DBAL\Schema\SchemaException;
 use Doctrine\ORM\Tools\Event\GenerateSchemaEventArgs;
+use Doctrine\ORM\Tools\ToolEvents;
 use Symfony\Component\DependencyInjection\Attribute\AutoconfigureTag;
 
 /** @codeCoverageIgnore */
 #[AutoconfigureTag(
     name: 'doctrine.event_listener',
-    attributes: ['event' => 'postGenerateSchema'],
+    attributes: ['event' => ToolEvents::postGenerateSchema],
 )]
 readonly class FixSequenceSchemaSubscriber
 {
--- src/Common/EventSubscriber/FixSchemaSubscriber.php
+++ src/Common/EventSubscriber/FixSchemaSubscriber.php
@@ -4,21 +4,18 @@ declare(strict_types=1);
 
 namespace App\Common\EventSubscriber;
 
-use Doctrine\Common\EventSubscriber;
 use Doctrine\DBAL\Schema\SchemaException;
 use Doctrine\ORM\Tools\Event\GenerateSchemaEventArgs;
 use Doctrine\ORM\Tools\ToolEvents;
+use Symfony\Component\DependencyInjection\Attribute\AutoconfigureTag;
 
 /** @codeCoverageIgnore */
-class FixSchemaSubscriber implements EventSubscriber
+#[AutoconfigureTag(
+    name: 'doctrine.event_listener',
+    attributes: ['event' => ToolEvents::postGenerateSchema],
+)]
+class FixSchemaSubscriber
 {
-    public function getSubscribedEvents(): array
-    {
-        return [
-            ToolEvents::postGenerateSchema,
-        ];
-    }
-
     /** @throws SchemaException */
     public function postGenerateSchema(GenerateSchemaEventArgs $args): void
     {
