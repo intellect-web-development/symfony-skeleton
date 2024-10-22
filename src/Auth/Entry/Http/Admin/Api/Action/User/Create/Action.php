aa0b562d2eda29c6811ead652b5656a8bb20fafd
alexander <sanyaerin@mail.ru>
2024-10-22 11:14:49 +0300

    wip

--- src/Auth/Entry/Http/Admin/Api/Action/User/Create/Action.php
+++ src/Auth/Entry/Http/Admin/Api/Action/User/Create/Action.php
@@ -5,14 +5,11 @@ declare(strict_types=1);
 namespace App\Auth\Entry\Http\Admin\Api\Action\User\Create;
 
 use App\Auth\Application\User\UseCase\Create\Handler;
-use App\Auth\Domain\User\User;
 use App\Auth\Entry\Http\Admin\Api\Contract\User\CommonOutputContract;
 use App\Common\Exception\Domain\DomainException;
 use IWD\SymfonyEntryContract\Dto\Input\OutputFormat;
-use IWD\SymfonyEntryContract\Dto\Output\ApiFormatter;
 use Nelmio\ApiDocBundle\Annotation\Model;
-use Nelmio\ApiDocBundle\Annotation\Security;
-use OpenApi\Annotations as OA;
+use OpenApi\Attributes as OA;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -20,56 +17,57 @@ class Action
 {
     public const NAME = 'api_admin_app_auth_user_create';
 
-    /**
-     * @OA\Tag(name="Auth.User")
-     * @OA\Post(
-     *     @OA\RequestBody(
-     *         @OA\MediaType(
-     *             mediaType="application/json",
-     *             @OA\Schema(
-     *                 ref=@Model(type=InputContract::class)
-     *             )
-     *         )
-     *     )
-     * )
-     * @OA\Response(
-     *     response=201,
-     *     description="Create command for User",
-     *     @OA\JsonContent(
-     *         allOf={
-     *             @OA\Schema(ref=@Model(type=ApiFormatter::class)),
-     *             @OA\Schema(
-     *                 type="object",
-     *                 @OA\Property(
-     *                     property="data",
-     *                     ref=@Model(type=CommonOutputContract::class)
-     *                 ),
-     *                 @OA\Property(
-     *                     property="status",
-     *                     example="201"
-     *                 )
-     *             )
-     *         }
-     *     )
-     *  )
-     * @OA\Response(
-     *     response=400,
-     *     description="Bad Request"
-     * ),
-     * @OA\Response(
-     *     response=401,
-     *     description="Unauthenticated",
-     * ),
-     * @OA\Response(
-     *     response=403,
-     *     description="Forbidden"
-     * ),
-     * @OA\Response(
-     *     response=404,
-     *     description="Resource Not Found"
-     * )
-     * @Security(name="Bearer")
-     */
+    #[OA\Tag(name: 'Auth.User')]
+    #[OA\Post(
+        requestBody: new OA\RequestBody(
+            description: "Create command for User",
+            required: true,
+            content: new OA\MediaType(
+                mediaType: 'application/json',
+                schema: new OA\Schema(
+                    ref: new Model(type: InputContract::class),
+                ),
+            )
+        )
+    )]
+    #[OA\Response(
+        response: 201,
+        description: 'User success created',
+        content: new OA\JsonContent(
+            properties: [
+                new OA\Property(
+                    property: 'status',
+                    type: 'integer',
+                    example: 201
+                ),
+                new OA\Property(
+                    property: 'ok',
+                    type: 'boolean',
+                    example: true
+                ),
+                new OA\Property(
+                    property: 'data',
+                    ref: new Model(type: CommonOutputContract::class)
+                ),
+                new OA\Property(
+                    property: 'messages',
+                    type: 'array',
+                    items: new OA\Items(
+                        type: 'string',
+                    )
+                )
+            ],
+            type: 'object'
+        )
+    )]
+    #[OA\Response(
+        response: 400,
+        description: 'Bad Request',
+    )]
+    #[OA\Response(
+        response: 401,
+        description: 'Unauthorized',
+    )]
     #[Route(
         path: '/api/admin/auth/users/create.{_format}',
         name: self::NAME,
